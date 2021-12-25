025cf42c354ff2a88e5a741bf14fbf3b5200db10
ruslan <kuzrus31@gmail.com>
2021-12-25 13:27:16 +0300

    hm-2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,98 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router();
+const Joi = require("joi");
+
+
+const contactsOperations = require('../../model/index')
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await contactsOperations.listContacts()
+    res.json({ contacts })
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId
+    } = req.params;
+    const contacts = await contactsOperations.getContactById(contactId)
+    if (!contacts) {
+      return res.status(404).json({ message: 'Not Found' })
+    }
+    res.json(contacts)
+  } catch (err) {
+    next(err)
+  }
+
+
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+
+  const postSchema = Joi.object({
+    name: Joi.string().min(2).required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
+  });
+  const { error } = postSchema.validate(req.body);
+
+  try {
+    if (error) {
+
+      error.status = 400;
+      throw error
+    }
+
+    const contacts = await contactsOperations.addContact(req.body);
+    res.status(201).json({ contacts })
+
+  } catch (err) {
+
+    next(err)
+  }
+
+
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const contacts = await contactsOperations.removeContact(contactId);
+
+    if (!contacts) {
+
+      return res.status(404).json({ message: "Not Found" })
+    }
+
+    res.status(200).json({ message: "delete contact" })
+
+  } catch (err) {
+    next(err)
+  }
+
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:contactId', async (req, res, next) => {
+  if (error) {
+
+    error.status = 400;
+    throw error
+  }
+  const { contactId
+  } = req.params;
+  const contacts = await contactsOperations.updateContact(contactId,
+    req.body);
+  if (!contacts) {
+    return res.status(404).json({ message: 'Not found' });
+  }
+  res.status().json({ contacts });
+} catch (error) {
+  next(error);
+}
+
 })
 
 module.exports = router
